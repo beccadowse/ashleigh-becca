18a6bcf781362549e501e8a5299c748fa3fba366
beccadowse <beccadowse@gmail.com>
2017-10-30 22:04:54 +0000

    added css and some Jango to the html

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ def hello (): #a function that returns hello world
 
 @app.route("/foods", methods=["POST"])
 def show_veg_restaurants():
-
 	form_data = request.form
+	diet = request.form.get('diet')
+	firstname = request.form.get('firstname')
 	callAPI = requests.post("https://maps.googleapis.com/maps/api/place/textsearch/json?query=Vegetarian&sensor=true&location=51.50020.1332&radius=20&type=restaurant&keyword=vegetarian&key=AIzaSyCnQcaSqXzBMfcO_4eWK8Oh1B3gFUgKof8",
 	data={"type": [form_data["diet"]]})
 	print form_data["diet"]
 	print callAPI.text
-	return render_template("foods.html")
+	return render_template('foods.html', variable="food")
 
 app.run(debug=True)
